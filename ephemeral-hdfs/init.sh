db05d4834ac7c36be8a3dd574a9517783a61e9d1
Rick Fletcher <fletch@pobox.com>
2018-01-22 13:54:36 -0500

    Account for not having hadoop native libs in the AMI

--- ephemeral-hdfs/init.sh
+++ ephemeral-hdfs/init.sh
@@ -10,6 +10,10 @@ if [ -d "ephemeral-hdfs" ]; then
   return 0
 fi
 
+DEBIAN_FRONTEND=noninteractive sudo apt-get install \
+  --assume-yes --allow-downgrades --allow-remove-essential --allow-change-held-packages \
+  libsnappy1v5
+
 case "$HADOOP_MAJOR_VERSION" in
   1)
     wget http://s3.amazonaws.com/spark-related-packages/hadoop-1.0.4.tar.gz
@@ -21,16 +25,19 @@ case "$HADOOP_MAJOR_VERSION" in
     cp /spark-home/hadoop-native/* /spark-home/ephemeral-hdfs/lib/native/
     ;;
   2) 
+    wget http://archive.apache.org/dist/hadoop/common/hadoop-2.4.1/hadoop-2.4.1.tar.gz
     wget http://s3.amazonaws.com/spark-related-packages/hadoop-2.0.0-cdh4.2.0.tar.gz  
     echo "Unpacking Hadoop"
-    tar xvzf hadoop-*.tar.gz > /tmp/spark-ec2_hadoop.log
+    tar xvzf hadoop-2.4.1.tar.gz > /tmp/spark-ec2_hadoop.log
+    tar xvzf hadoop-2.0.0-cdh4.2.0.tar.gz >> /tmp/spark-ec2_hadoop.log
     rm hadoop-*.tar.gz
     mv hadoop-2.0.0-cdh4.2.0/ ephemeral-hdfs/
 
     # Have single conf dir
     rm -rf /spark-home/ephemeral-hdfs/etc/hadoop/
     ln -s /spark-home/ephemeral-hdfs/conf /spark-home/ephemeral-hdfs/etc/hadoop
-    cp /spark-home/hadoop-native/* /spark-home/ephemeral-hdfs/lib/native/
+    cp hadoop-2.4.1/lib/native/* /spark-home/ephemeral-hdfs/lib/native/
+    ln -sf /usr/lib/x86_64-linux-gnu/libsnappy.so.1 /spark-home/ephemeral-hdfs/lib/native/.
     ;;
   yarn)
     wget http://s3.amazonaws.com/spark-related-packages/hadoop-2.4.0.tar.gz
